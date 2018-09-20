97e0e3a15ff6f468e786eb0ac4a1c4dee31534e2
daisukelab <niz.edge@gmail.com>
2018-09-20 13:09:49 +0900

    Add BigH5Array feature to be more _array_.

--- big_h5_array.py
+++ big_h5_array.py
@@ -3,20 +3,25 @@ import tables
 import numpy as np
 
 class BigH5Array():
-    def __init__(self, filename, row_size, atom=tables.Float32Atom()):
+    def __init__(self, filename, shape, atom=tables.Float32Atom()):
         self.filename = filename
-        self.row_size = row_size
+        self.shape = shape
         self.atom = atom
     def open_for_write(self):
         self.f = tables.open_file(self.filename, mode='w')
-        self.array_c = self.f.create_earray(self.f.root, 'data', self.atom, (0, self.row_size))
+        self.array_c = self.f.create_carray(self.f.root, 'carray', self.atom, self.shape)
+    def open_for_write_expandable(self):
+        self.f = tables.open_file(self.filename, mode='w')
+        self.array_e = self.f.create_earray(self.f.root, 'data', self.atom, (0, self.shape[1]))
     def open_for_read(self):
         self.f = tables.open_file(self.filename, mode='r')
-    def data(self):
+    def data(self): # for expandable
         # bigarray.data()[1:10,2:20]
         return self.f.root.data
-    def write(self, row_data):
-        self.array_c.append(row_data)
+    def append(self, row_data): # for expandable
+        self.array_e.append(row_data)
+    def __call__(self): # for random access
+        return self.f.root.carray
     def close(self):
         self.f.close()
 
@@ -27,21 +32,46 @@ if __name__ == '__main__':
         ROW_SIZE = 2*2*1056
         COL_SIZE = 200
 
-        def test_read_write(self):
+        def test_1_as_normal_array(self):
+            # write test - just confirm no error
+            array = BigH5Array('test2.h5', (TestBigH5Array.COL_SIZE, TestBigH5Array.ROW_SIZE))
+            self.opened_file = array
+            array.open_for_write()
+            x = np.random.rand(1, TestBigH5Array.COL_SIZE, TestBigH5Array.ROW_SIZE)
+            self.test_data = x[0]
+            array()[...] = x
+            array.close()
+
+            # read test - real test
+            array = BigH5Array('test2.h5', (TestBigH5Array.COL_SIZE, TestBigH5Array.ROW_SIZE))
+            self.opened_file = array
+            array.open_for_read()
+            self.test_data = np.array(self.test_data)
+            for col in range(TestBigH5Array.COL_SIZE):
+                print('Testing ...[%d]' % col, self.test_data.shape, self.test_data[col])
+                row = 0
+                for a, b in zip(array()[col], self.test_data[col]):
+                    self.assertAlmostEqual(a, b, msg='fails at [{},{}]'.format(col, row))
+                    row += 1
+            array.close()
+
+            self.opened_file = None
+
+        def test_2_write_expandable(self):
             # write test - just confirm no error
             self.test_data = []
-            writer = BigH5Array('test2.h5', TestBigH5Array.ROW_SIZE)
+            writer = BigH5Array('test2.h5', (TestBigH5Array.COL_SIZE, TestBigH5Array.ROW_SIZE))
             self.opened_file = writer
-            writer.open_for_write()
+            writer.open_for_write_expandable()
             for col in range(TestBigH5Array.COL_SIZE):
                 x = np.random.rand(1, TestBigH5Array.ROW_SIZE)
                 self.test_data.append(x[0])
-                writer.write(x)
+                writer.append(x)
             writer.close()
 
             # read test - real test
-            reader = BigH5Array('test2.h5', TestBigH5Array.ROW_SIZE)
-            self.opened_file = writer
+            reader = BigH5Array('test2.h5', (TestBigH5Array.COL_SIZE, TestBigH5Array.ROW_SIZE))
+            self.opened_file = reader
             reader.open_for_read()
             self.test_data = np.array(self.test_data)
             for col in range(TestBigH5Array.COL_SIZE):
